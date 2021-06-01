1d7fe46b3ae71e4d1a2d19dbb6c707e6e912b25d
Aidar <aidarxan@yahoo.com>
2021-06-01 21:26:34 +0400

    homework1

--- app/app/src/main/java/kz/gk/app/MainActivity2.java
+++ app/app/src/main/java/kz/gk/app/MainActivity2.java
@@ -6,18 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-
-        Button button1 = findViewById(R.id.button1);
-
+        Button button1 = findViewById(R.id.button2);
         button1.setOnClickListener(v -> {
-            Intent intent1 = new Intent(v.getContext(), MainActivity2.class);
+            Intent intent1 = new Intent(v.getContext(),MainActivity.class);
             startActivity(intent1);
         });
     }
